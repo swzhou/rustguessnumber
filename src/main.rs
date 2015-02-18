f67634ba132a303946d42cc3a1293aa44d3d85d6
swzhou <swing.zhou@hotmail.com>
2015-02-18 08:22:34 +0800

    finish the game when win

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,30 @@ fn main() {
     println!("Guess the number!");
     let secret_number = (rand::random::<u32>() % 100) + 1;
     println!("The secret number is: {}", secret_number);
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
     
-    let input = old_io::stdin().read_line()
+        let input = old_io::stdin().read_line()
                             .ok()
                             .expect("Failed to read line");
-    let input_number: Result<u32, _> = input.trim().parse(); 
-    let num = match input_number {
-        Ok(num) => num,
-        Err(_) => {
-            println!("Please input a number!");
-            return;
-        }
-    };
-    println!("You guessed: {}", num);    
+        let input_number: Result<u32, _> = input.trim().parse(); 
+        let num = match input_number {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input a number!");
+                return;
+            }
+        };
+        println!("You guessed: {}", num);    
     
-    match cmp(num, secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match cmp(num, secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                return;
+            },
+        }
     }
 }
 
