65a26b114603d4c07f818d385af3524a6f321e90
swzhou <swing.zhou@hotmail.com>
2015-02-18 08:18:48 +0800

    compare guess number and secret number

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
 use std::old_io;
+use std::rand;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
+    let secret_number = (rand::random::<u32>() % 100) + 1;
+    println!("The secret number is: {}", secret_number);
     println!("Please input your guess.");
     
     let input = old_io::stdin().read_line()
                             .ok()
                             .expect("Failed to read line");
-    println!("You guessed: {}", input);
+    let input_number: Result<u32, _> = input.trim().parse(); 
+    let num = match input_number {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Please input a number!");
+            return;
+        }
+    };
+    println!("You guessed: {}", num);    
+    
+    match cmp(num, secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+}
+
+fn cmp(a: u32, b: u32) -> Ordering {
+    if a < b {Ordering::Less}
+    else if a > b {Ordering::Greater}
+    else {Ordering::Equal}
 }
